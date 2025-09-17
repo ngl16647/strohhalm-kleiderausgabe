88c4fbec8dd506dee011e26dafc6ea66c5755321
Roy-j-Xu <ruoyu.xv@gmail.com>
2025-09-17 17:35:29 +0200

    Adds middlewares

---
+++ backend/middlewares/middlewares.go
@@ -0,0 +1,60 @@
+package middlewares
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func InitGlobalMiddlewares(r chi.Router) {
+	r.Use(middleware.Logger)
+	r.Use(APIKeyAuth("12345"))
+}
+
+func APIKeyAuth(validKey string) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			authHeader := r.Header.Get("Authorization")
+
+			// Expect "Bearer <key>"
+			const prefix = "Bearer "
+			if !strings.HasPrefix(authHeader, prefix) {
+				http.Error(w, "missing or invalid API key", http.StatusUnauthorized)
+				return
+			}
+
+			apiKey := strings.TrimPrefix(authHeader, prefix)
+			if apiKey != validKey {
+				http.Error(w, "unauthorized", http.StatusUnauthorized)
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func AdminKeyAuth(validKey string) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			authHeader := r.Header.Get("Admin-Authorization")
+
+			// Expect "Bearer <key>"
+			const prefix = "Bearer "
+			if !strings.HasPrefix(authHeader, prefix) {
+				http.Error(w, "missing or invalid API key", http.StatusUnauthorized)
+				return
+			}
+
+			apiKey := strings.TrimPrefix(authHeader, prefix)
+			if apiKey != validKey {
+				http.Error(w, "unauthorized", http.StatusUnauthorized)
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
