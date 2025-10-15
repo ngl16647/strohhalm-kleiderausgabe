7a0e2b5c4c53c60b2fb2218f10e8b456815e5811
Roy-j-Xu <ruoyu.xv@gmail.com>
2025-10-15 20:42:16 +0200

    Change port behavior on autocert

--- backend/cfg/autocert.go
+++ backend/cfg/autocert.go
@@ -3,24 +3,35 @@ package cfg
 import (
 	"crypto/tls"
 	"log"
+	"net/http"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"golang.org/x/crypto/acme/autocert"
 )
 
-func GenerateTlsConfig(cfg *Config) *tls.Config {
+func GenerateTlsConfig(cfg *Config) (*autocert.Manager, *tls.Config) {
 	m := &autocert.Manager{
 		Prompt:     autocert.AcceptTOS,
 		HostPolicy: autocert.HostWhitelist(cfg.Tls.Autocert.Domains...),
 		Cache:      autocert.DirCache(cfg.Tls.Autocert.Cache),
 		Email:      cfg.Tls.Autocert.Email,
 	}
-	validateAutocert(m, cfg.Tls.Autocert.Domains)
-	return &tls.Config{
+	return m, &tls.Config{
 		GetCertificate: m.GetCertificate,
 	}
 }
 
-func validateAutocert(m *autocert.Manager, domains []string) {
+func HttpServer(m *autocert.Manager) *chi.Mux {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Handle("/*", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		m.HTTPHandler(nil).ServeHTTP(w, r)
+	}))
+	return r
+}
+
+func ValidateAutocert(m *autocert.Manager, domains []string) {
 	for _, domain := range domains {
 		_, err := m.GetCertificate(&tls.ClientHelloInfo{
 			ServerName: domain,
--- backend/main.go
+++ backend/main.go
@@ -8,6 +8,7 @@ import (
 	"strohhalm-backend/db"
 	"strohhalm-backend/middlewares"
 	"strohhalm-backend/routes"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -29,7 +30,6 @@ func main() {
 }
 
 func runServer(r chi.Router) {
-	addr := fmt.Sprintf("%s:%d", cfg.GlobalConfig.Server.Host, cfg.GlobalConfig.Server.Port)
 
 	if !cfg.GlobalConfig.Api.UseApiKey {
 		log.Println("WARNING: Server running without API key verification")
@@ -37,23 +37,47 @@ func runServer(r chi.Router) {
 
 	var err error
 	if !cfg.GlobalConfig.Tls.UseTls {
+		addr := Addr(cfg.GlobalConfig.Server.Host, cfg.GlobalConfig.Server.Port)
 		log.Println("WARNING: Server running without TLS")
 		log.Printf("Server started on port %s\n", addr)
 		err = http.ListenAndServe(addr, r)
 	} else if cfg.GlobalConfig.Tls.Autocert.UseAutocert { // autocert with Let's Encrypt
+		addr := Addr(cfg.GlobalConfig.Server.Host, 443) // must serve backend on 443
+		acManager, tlsCfg := cfg.GenerateTlsConfig(cfg.GlobalConfig)
 		httpsServer := &http.Server{
 			Addr:      addr,
 			Handler:   r,
-			TLSConfig: cfg.GenerateTlsConfig(cfg.GlobalConfig),
+			TLSConfig: tlsCfg,
 		}
-		log.Printf("Server started on port %s\n", addr)
+
+		// listening for ACME challenges
+		httpServer := cfg.HttpServer(acManager)
+		go func() {
+			log.Fatal(http.ListenAndServe(Addr(cfg.GlobalConfig.Server.Host, 80), httpServer))
+		}()
+
+		// check if certificates exist
+		<-time.After(time.Second * 2)
+		cfg.ValidateAutocert(acManager, cfg.GlobalConfig.Tls.Autocert.Domains)
+
+		log.Printf("Server started on port %s with autocert\n", addr)
 		err = httpsServer.ListenAndServeTLS("", "")
 	} else { // manual TLS
+		addr := Addr(cfg.GlobalConfig.Server.Host, cfg.GlobalConfig.Server.Port)
 		log.Printf("Server started on port %s\n", addr)
-		err = http.ListenAndServeTLS(addr, cfg.GlobalConfig.Tls.CertFile, cfg.GlobalConfig.Tls.KeyFile, r)
+		err = http.ListenAndServeTLS(
+			addr,
+			cfg.GlobalConfig.Tls.CertFile,
+			cfg.GlobalConfig.Tls.KeyFile,
+			r,
+		)
 	}
 
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func Addr(host string, port int) string {
+	return fmt.Sprintf("%s:%d", host, port)
 }
